b0b79e9974eb574387106c1806c21fe8669e4767
nkalu11 <teachers@10.180.7.104>
2014-06-25 10:33:44 -0700

    test

---
+++ Homework/test/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: teachers
+ *
+ * Created on June 25, 2014, 10:32 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"hello world"<<endl;
+    return 0;
+}
+
