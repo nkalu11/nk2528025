18487ce8a5c84c82084878eef4b99a029b030ba4
nkalu11 <teachers@10.180.7.104>
2014-06-25 10:29:55 -0700

    testing github

---
+++ test/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: teachers
+ *
+ * Created on June 25, 2014, 10:29 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
