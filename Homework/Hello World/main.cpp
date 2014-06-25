45bfbacf04aea791fa56a49abc147e7b0677f813
nkalu11 <teachers@10.180.7.104>
2014-06-25 11:03:05 -0700

    test

---
+++ Homework/Hello World/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:  Hello World
+ * Author: Nnamdi Kalu
+ *Created on June 25, 2014, 10:57 AM
+ *Purpose First Program
+ */
+
+//System Level Libraries
+
+#include <iostream>
+
+using namespace std;
+
+//System Level Preferences
+
+//Global Constants
+
+//Execution Begins Here
+
+int main(int argc, char** argv) {
+    
+//Print Simple Text
+    cout <<"Hello World"<<endl;
+    //Exit Stage Right
+    return 0;
+}
+
