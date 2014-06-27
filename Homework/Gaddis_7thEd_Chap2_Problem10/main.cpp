a518d79b65e03319b6e8fcaf74fb22b71153ed99
nkalu11 <teachers@10.180.7.104>
2014-06-26 21:11:54 -0700

    test

---
+++ Homework/Gaddis_7thEd_Chap2_Problem10/main.cpp
@@ -0,0 +1,45 @@
+/* File: Gaddis 7th Chap2 Problem 16
+ * Author: Nnamdi Kalu
+ * Created on June 26, 2014, 12:40 PM
+ * Purpose 10th problem on homework from Gaddis 7thED problem 16
+ */
+
+//System Level Libraries
+
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constant
+
+//Function Prototypes
+
+//Execution Begins Here
+
+int main(int argc, char** argv) {
+
+    //Set variables equal to stars
+    string a = "   *   ";
+    string b = "  ***  ";
+    string c = " ***** ";
+    string d = "*******";   
+    string e = " ***** ";
+    string f = "  ***  ";
+    string g = "   *   ";
+    
+    //Print out variables 
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;      
+    cout<<e<<endl;
+    cout<<f<<endl;
+    cout<<g<<endl;  
+    
+    //Exit stage right         
+            
+    return 0;
+}
+
