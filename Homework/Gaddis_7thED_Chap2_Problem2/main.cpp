ab6710497e7ecd3d9bf0d719f4b1b8a14f31039a
nkalu11 <teachers@10.180.7.104>
2014-06-26 21:09:37 -0700

    test

---
+++ Homework/Gaddis_7thED_Chap2_Problem2/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:  Gaddis_7thED_Chap2_Prob2
+ * Author: teachers
+ *Created on June 24, 2014, 8:25 PM
+ *Purpose Second Problem
+ */
+
+// System Level Libraries
+
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constant
+
+//Function Prototypes
+
+//Execution Begins Here
+
+int main(int argc, char** argv) {
+
+    float ansales= 4.6e6;//Annual sales is 4.6 million dollars
+    float percent = .62;//Profit is 62% of annual sales
+    float profit = ansales * percent;
+    
+    
+    cout<< profit<<endl;
+    //Exit stage left
+    return 0;
+}
+
