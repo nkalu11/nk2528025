7d421114a8f12c537ab1be15648aa084ac9b79a7
nkalu11 <teachers@10.180.7.104>
2014-06-26 21:17:24 -0700

    test

---
+++ Homework/GADDIS _7thEd_Chap2_Problem1/main.cpp
@@ -0,0 +1,29 @@
+/* File:  Gaddis 7thEd Chap2 Prob1
+ * Author: Nnamdi Kalu
+ *Created on June 24, 2014, 7:02 PM
+ * purpose homework problem 1
+ */
+
+//System Level Libraries
+
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constant
+
+//Function Prototypes
+
+//Execution Begins Here
+
+int main(int argc, char** argv) {
+    int dog= 62;
+    int cat= 99;
+    int total= dog+cat;
+    //Output Simple Text
+    cout<< total<< endl;
+    //Exit Stage Right
+    return 0;
+}
