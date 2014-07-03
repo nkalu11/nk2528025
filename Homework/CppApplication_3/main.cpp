5dcbf90cdad26b299c033fd8e3bc718145e039f5
nkalu11 <nkalu11@yahoo.com>
2014-07-03 00:57:53 -0800

    hmwk

---
+++ Homework/CppApplication_3/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: Nnamdi
+ *
+ * Created on July 3, 2014, 12:21 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+   cout<<"hello world"<<endl;
+
+    return 0;
+}
+
