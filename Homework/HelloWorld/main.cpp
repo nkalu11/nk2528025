ea64aab6a394ba580f7674dfe5daf517a78fe7fa
nkalu11 <nkalu11@yahoo.com>
2014-07-24 13:31:56 -0800

    Signed-off-by: nkalu11 <nkalu11@yahoo.com>

---
+++ Homework/HelloWorld/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: Nnamdi
+ *
+ * Created on July 24, 2014, 1:30 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    cout<<"Hello World"<<endl;
+
+    return 0;
+}
+
