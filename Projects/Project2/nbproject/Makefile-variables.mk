8ba7fc15ec32dd7beb326219a7821ae2ed0cf761
nkalu11 <nkalu11@yahoo.com>
2014-07-27 20:52:28 -0800

    Signed-off-by: nkalu11 <nkalu11@yahoo.com>

--- Projects/Project2/nbproject/Makefile-variables.mk
+++ Projects/Project2/nbproject/Makefile-variables.mk
@@ -9,16 +9,16 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Debug=project_2
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/project_2
+CND_ARTIFACT_NAME_Debug=project2
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/project2
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
 CND_PACKAGE_NAME_Debug=project2.tar
 CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/project2.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Release=project_2
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/project_2
+CND_ARTIFACT_NAME_Release=project2
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/project2
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
 CND_PACKAGE_NAME_Release=project2.tar
 CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/project2.tar
